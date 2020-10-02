2b88247e7b782b5c78a43c65af5fd38c24c37d7d
William Kim <ddong3525@gmail.com>
2020-10-03 02:12:56 +0900

    Logout button functional implementation

---
+++ project_mlb/src/components/LogoutModal.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Modal, Button} from 'react-bootstrap';
+
+class LogoutModal extends Component{
+    render(){
+        console.log("LogoutModal props:", this.props);
+        return(
+            <Modal
+                show={this.props.show}
+                onHide={this.props.handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                <Modal.Title>Are you sure you want to Logout?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                Click yes to logout
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={this.props.handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={this.props.handleClose}>Yes</Button>
+                </Modal.Footer>
+            </Modal>
+        )
+    }
+}
+
+export default LogoutModal;
--- project_mlb/src/components/NavigationBar.js
+++ project_mlb/src/components/NavigationBar.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import { Navbar, Nav, NavItem, Button, Image} from "react-bootstrap";
 import { Link} from "react-router-dom";
-
+import LogoutModal from './LogoutModal';
 const bookIcon = require('../icons/book.png');
 
-
 class MlbNavbar extends Component {
+  state={
+    show : false,
+  }
+  handleClose = () => {
+    this.setState({show: false});
+  }
+  handleOpen = () => {
+    this.setState({show: true});
+  }
+
   render() {
-    return (
+     return (
       <div>
         <div style={{background: "#56a2b5"}}>
-            
             <h1 style={{margin: 0, padding: "25px", textAlign: "left", fontFamily: "fantasy", color: "white", fontWeight: "bold" }}><img src= {bookIcon} style={{width: "70px", marginRight: "600px"}} alt="this is book" /> My Little Bookstore</h1>
         </div>
+        <LogoutModal show={this.state.show} handleClose={this.handleClose}/>
         <Navbar style={{background: "#2E7384"}} >
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -22,9 +31,8 @@ class MlbNavbar extends Component {
               <NavItem>  <Link className="nav-link" style={{color: "white", fontWeight: "bold", fontSize: "20px"}} to="/MyBookstore">My Bookstore</Link> </NavItem>
               <NavItem>  <Link className="nav-link" style={{color: "white", fontWeight: "bold", fontSize: "20px"}} to="/Discussion">Discussion</Link> </NavItem>
               <NavItem>  <Link className="nav-link" style={{color: "white", fontWeight: "bold", fontSize: "20px"}} to="/Profile">Profile</Link> </NavItem>
-              
             </Nav>
-            <Button>Logout</Button>
+            <Button onClick={this.handleOpen}>Logout</Button>
 
           </Navbar.Collapse>
         </Navbar>
