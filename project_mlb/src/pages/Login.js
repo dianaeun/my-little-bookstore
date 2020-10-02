3ba0a214ea7eadd2840cbd8a29081089fdc5f0c1
William Kim <ddong3525@gmail.com>
2020-10-03 04:17:53 +0900

    Login state

--- project_mlb/src/pages/Login.js
+++ project_mlb/src/pages/Login.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 
 class Login extends Component{
+    state = {
+        isLogin: true
+    }
     render(){
         return (
             <h1> Hi I'm Login Page!</h1>
+
         )
     }
 }
