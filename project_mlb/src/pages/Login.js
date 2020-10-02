0adfe6a7d82e84e06cb7c515e734e75c6124da78
Hyeon Joon Lee <hyeonjoon.lee@stonybrook.edu>
2020-10-03 04:30:55 +0900

    test

--- project_mlb/src/pages/Login.js
+++ project_mlb/src/pages/Login.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class Login extends Component{
     render(){
         return (
+            <div>
             <h1> Hi I'm Login Page!</h1>
+            <h1> Hyeon Joon Lee</h1>
+            </div>
         )
     }
 }
