a7495349465cd3cce89c9e99331e49730888391c
diana <dianaeun98@gmail.com>
2020-10-04 20:22:23 +0900

    Update Browse.js
    Design updates on Browse.js

--- project_mlb/src/pages/Browse.js
+++ project_mlb/src/pages/Browse.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Table from 'react-bootstrap/Table';
 import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import {Form, InputGroup, FormControl, Dropdown, DropdownButton, Jumbotron, Container, Col} from 'react-bootstrap';
 
 const star = require("../icons/star.png");
 const blankStar = require("../icons/blank_star.png");
@@ -20,18 +20,10 @@ class Browse extends Component{
             stars.push(<img src={blankStar} alt="star" style={{ width: "22px" }} />);
         }
         return <td>{stars}</td>
-      }
+    }
 
     render(){
         var genres = ['Romance', 'Horror', 'Fantasy', 'Adventure', 'Science']
-
-        const searchGenres = []
-
-        for (const [_index, value] of genres.entries()) {
-            searchGenres.push(<input type="checkbox" value={value} name="genre"/>)
-            searchGenres.push(<label>{value}</label>)
-        }
-
 
         // dummy data for book information
         var book1 = ['Introduction To Java Programming', 'Y. Daniel Liang', 5, 3.50]
@@ -54,31 +46,57 @@ class Browse extends Component{
 
         return (
             <div>
-                <span>
-                    <input type="text" id="search" name="search"></input>
-                    <Button
-                    title="Search"
-                    onClick={this.searchBook()}
-                    />
-                </span>
-                <span>
-                <h4>Advanced Search</h4>
-                <label>Genre</label>
-                {searchGenres}
-                <br></br>
-                <label>Location</label>
-                <input type="checkbox" value="nearMe" name="nearMe" /> Near Me
+                <Jumbotron style={{backgroundColor:'lightgray'}}>
+                    <Container>
+                    <InputGroup>
+                        <FormControl
+                        placeholder="Search Term"
+                        aria-label="searchbar"
+                        aria-describedby="basic-addon2"
+                        />
+                        <DropdownButton
+                        as={InputGroup.Append}
+                        variant="outline-secondary"
+                        title="Search"
+                        id="input-group-dropdown-2"
+                        >
+                        <Dropdown.Item href="#">Whole</Dropdown.Item>
+                        <Dropdown.Item href="#">Title</Dropdown.Item>
+                        <Dropdown.Item href="#">Author</Dropdown.Item>
+                        <Dropdown.Item href="#">ISBN</Dropdown.Item>
+                        </DropdownButton>
+                    </InputGroup>
+                    <br></br>
+                    <Form>
+                        <Form.Label column sm="5">Advanced Search</Form.Label>
+                        <Form.Row>
+                            <Col className="mb-3">
+                            <Form.Label column sm="2">Genre</Form.Label>
+                            {genres.map((gen) => (
+                                <Form.Check inline label={gen} type='checkbox' id={gen} />
+                            ))}
+                            </Col>
+                        </Form.Row>
+                        <Form.Row>
+                        <Col className="mb-3">
+                            <Form.Label column sm="2">Location</Form.Label>
+                            <Form.Check inline label='Near Me' type='checkbox' id='nearme' />
+                            </Col>
+                        </Form.Row>
+                    </Form>
+                    </Container>
+                </Jumbotron>
 
-                </span>
-
-
-                <span>
-                <h3>Resulting Books</h3>
-                <label>Sort By</label>
-                <input type="radio" value="Rating" name="sortBy" /> Rating 
-                <input type="radio" value="Price" name="sortBy" /> Price 
-                <input type="radio" value="Alphabet" name="sortBy" /> Alphabet
-                </span>
+                
+                <Form>
+                    <h5>&nbsp;&nbsp;Search Results</h5>
+                    <div key={`inline-radio`} className="mb-3" style={{textAlign: "right"}}>
+                        <Form.Label column sm="1">Sort By</Form.Label>
+                        <Form.Check inline label='Rating' type='radio' id='rating' />
+                        <Form.Check inline label='Price' type='radio' id='price' />
+                        <Form.Check inline label='Alphabet' type='radio' id='alphabet' />
+                    </div>
+                </Form>
                 <Table size="sm" style={{ width: "1000px", marginLeft: "auto", marginRight: "auto"}}>
                     <thead>
                         <tr>
