a838191fedde6857db4cf0bcff08d3224c381c24
Hyeon Joon Lee <hyeonjoon.lee@stonybrook.edu>
2020-10-17 15:07:28 +0900

    approaches for handling requests updated

--- project_mlb/src/pages/MyBookstore.js
+++ project_mlb/src/pages/MyBookstore.js
@@ -24,14 +24,14 @@ class MyBookstore extends Component{
         bookSelected: null
     }
     books = [
-      { date: '2019/09/04', title: 'Harry Potter and the Philosopher', author: 'J.K. Rowling', price: 15.00, rate: 5 , publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "An extremely powerful story of a young Southern Negro,", hycomment: " from his late high school days through three years of college to his life in Harlem. His early training prepared him for a life of humility before white men, but through injustices- large and small, he came to realize that he was an invisible man. People saw in him only a reflection of their preconceived ideas of what he was, denied his individuality, and ultimately did not see him at all. This theme, which has implications far beyond the obvious racial parallel, is skillfully handled. The incidents of the story are wholly absorbing. The boys dismissal from college because of an innocent mistake, his shocked reaction to the anonymity of the North and to Harlem, his nightmare experiences on a one-day job in a paint factory and in the hospital, his lightning success as the Harlem leader of a communistic organization known as the Brotherhood, his involvement in black versus white and black versus black clashes and his disillusion and understanding of his invisibility- all climax naturally in scenes of violence and riot, followed by a retreat which is both literal and figurative. Parts of this experience may have been told before, but never with such freshness, intensity and power. This is Ellisons first novel, but he has complete control of his story and his style. Watch it.", Title2: "YOU. ARE. THE. DEAD. Oh my God. ", dayecomment: " YOU. ARE. THE. DEAD. Oh my God. I got the chills so many times toward the end of this book. It completely blew my mind. It managed to surpass my high expectations AND be nothing at all like I expected. Or in Newspeak Double Plus Good. Let me preface this with an apology. If I sound stunningly inarticulate at times in this review, I can't help it. My mind is completely fried. This book is like the dystopian Lord of the Rings, with its richly developed culture and economics, not to mention a fully developed language called Newspeak, or rather more of the anti-language, whose purpose is to limit speech and understanding instead of to enhance and expand it. The world-building is so fully fleshed out and spine-tinglingly terrifying that it's almost as if George travelled to such a place, escaped from it, and then just wrote it all down. I read Fahrenheit 451 over ten years ago in my early teens. At the time, I remember really wanting to read 1984, although I never managed to get my hands on it. I'm almost glad I didn't. Though I would not have admitted it at the time, it would have gone over my head. Or at the very least, I wouldn't have been able to appreciate it", request: 2},
-      { date: '2019/09/04',title: 'Harry Potter and the Chamber of Secrets', author: 'J.K. Rowling', price: 16.00, rate: 5 , publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "Exciting", hycomment: "This book is really interesting. I really recommend to buy this book!", Title2: "Fantasy is alwasys good", dayecomment: "Easy to read", request: 0},
-      { date: '2019/09/28',title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', price: 15.00, rate: 4, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "Do not forget?", hycomment: "Harry poter is impressive", Title2: "Still reading", dayecomment: "so interesting to read", request: 0},
-      { date: '2020/01/05',title: 'Harry Potter and the Order of the Phoenix', author: 'J.K. Rowling', price: 17.00, rate: 5, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "You won't regret", hycomment: "to buy Harry poter series", Title2: "Order this book now", dayecomment: "you won't regret", request: 1},
-      { date: '2020/05/17',title: 'Harry Potter and The Goblet of Fire', author: 'J.K. Rowling', price: 20.00, rate: 3 , publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "What am I going to talk ", hycomment: "...", Title2: "Nothing to say", dayecomment: "Incredible", request: 0},
-      { date: '2020/06/25',title: 'Harry Potter and the Half-Blood Prince', author: 'J.K. Rowling', price: 10.00, rate: 4, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A" , Title1: "who is the main character", hycomment: "Harry poter?", Title2: "No", dayecomment: "That is secret", request: 0},
-      { date: '2020/07/30',title: 'Harry Potter and the Deathly Hallows – Part 1', author: 'J.K. Rowling', price: 13.00, rate: 4, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A" , Title1: "Nothing to mention", hycomment: "No idea", Title2: "Let's skip", dayecomment: "okay?", request: 0},
-      { date: '2020/08/01',title: 'Harry Potter and the Deathly Hallows – Part 2', author: 'J.K. Rowling', price: 30.00, rate: 3, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A" , Title1: "Test!", hycomment: "Okay test", Title2: "Last commnet", dayecomment: "Success", request: 1},
+      { date: '2019/09/04', title: 'Harry Potter and the Philosopher', author: 'J.K. Rowling', price: 15.00, rate: 5 , publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "An extremely powerful story of a young Southern Negro,", hycomment: " from his late high school days through three years of college to his life in Harlem. His early training prepared him for a life of humility before white men, but through injustices- large and small, he came to realize that he was an invisible man. People saw in him only a reflection of their preconceived ideas of what he was, denied his individuality, and ultimately did not see him at all. This theme, which has implications far beyond the obvious racial parallel, is skillfully handled. The incidents of the story are wholly absorbing. The boys dismissal from college because of an innocent mistake, his shocked reaction to the anonymity of the North and to Harlem, his nightmare experiences on a one-day job in a paint factory and in the hospital, his lightning success as the Harlem leader of a communistic organization known as the Brotherhood, his involvement in black versus white and black versus black clashes and his disillusion and understanding of his invisibility- all climax naturally in scenes of violence and riot, followed by a retreat which is both literal and figurative. Parts of this experience may have been told before, but never with such freshness, intensity and power. This is Ellisons first novel, but he has complete control of his story and his style. Watch it.", Title2: "YOU. ARE. THE. DEAD. Oh my God. ", dayecomment: " YOU. ARE. THE. DEAD. Oh my God. I got the chills so many times toward the end of this book. It completely blew my mind. It managed to surpass my high expectations AND be nothing at all like I expected. Or in Newspeak Double Plus Good. Let me preface this with an apology. If I sound stunningly inarticulate at times in this review, I can't help it. My mind is completely fried. This book is like the dystopian Lord of the Rings, with its richly developed culture and economics, not to mention a fully developed language called Newspeak, or rather more of the anti-language, whose purpose is to limit speech and understanding instead of to enhance and expand it. The world-building is so fully fleshed out and spine-tinglingly terrifying that it's almost as if George travelled to such a place, escaped from it, and then just wrote it all down. I read Fahrenheit 451 over ten years ago in my early teens. At the time, I remember really wanting to read 1984, although I never managed to get my hands on it. I'm almost glad I didn't. Though I would not have admitted it at the time, it would have gone over my head. Or at the very least, I wouldn't have been able to appreciate it", request: []},
+      { date: '2019/09/04',title: 'Harry Potter and the Chamber of Secrets', author: 'J.K. Rowling', price: 16.00, rate: 5 , publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "Exciting", hycomment: "This book is really interesting. I really recommend to buy this book!", Title2: "Fantasy is alwasys good", dayecomment: "Easy to read", request: []},
+      { date: '2019/09/28',title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', price: 15.00, rate: 4, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "Do not forget?", hycomment: "Harry poter is impressive", Title2: "Still reading", dayecomment: "so interesting to read", request: [{sender: "Hyeon Joon Lee", date: "2020/05/30"}]},
+      { date: '2020/01/05',title: 'Harry Potter and the Order of the Phoenix', author: 'J.K. Rowling', price: 17.00, rate: 5, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "You won't regret", hycomment: "to buy Harry poter series", Title2: "Order this book now", dayecomment: "you won't regret", request: []},
+      { date: '2020/05/17',title: 'Harry Potter and The Goblet of Fire', author: 'J.K. Rowling', price: 20.00, rate: 3 , publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A", Title1: "What am I going to talk ", hycomment: "...", Title2: "Nothing to say", dayecomment: "Incredible", request: []},
+      { date: '2020/06/25',title: 'Harry Potter and the Half-Blood Prince', author: 'J.K. Rowling', price: 10.00, rate: 4, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A" , Title1: "who is the main character", hycomment: "Harry poter?", Title2: "No", dayecomment: "That is secret", request: [{sender: "Daye Eun", date: "2020/10/12"}]},
+      { date: '2020/07/30',title: 'Harry Potter and the Deathly Hallows – Part 1', author: 'J.K. Rowling', price: 13.00, rate: 4, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A" , Title1: "Nothing to mention", hycomment: "No idea", Title2: "Let's skip", dayecomment: "okay?", request: []},
+      { date: '2020/08/01',title: 'Harry Potter and the Deathly Hallows – Part 2', author: 'J.K. Rowling', price: 30.00, rate: 3, publisher: "Bloomsbury", isbn: "9781408883730", genre: "fantasy", description: "N/A" , Title1: "Test!", hycomment: "Okay test", Title2: "Last commnet", dayecomment: "Success", request: []},
     ];
     handleClose = () => {
       this.setState({deleteBook: false, addBook: false, editBook: false});
@@ -85,7 +85,6 @@ class MyBookstore extends Component{
                       <th style={{paddingLeft: "1rem"}}>edit</th>
                       <th style={{paddingLeft: "1rem"}}>del</th>
                       <th style={{paddingLeft: "2rem"}}>Requests</th>
-                      <th></th>
                     </tr>
                   </thead>
                   <tbody>
@@ -98,7 +97,7 @@ class MyBookstore extends Component{
                         <td style={{paddingTop: "0.5rem"}}><Link href="#">{book.author}</Link></td>
                         <td style={{paddingLeft: "2rem", paddingTop: "0.5rem"}}>${book.price}</td>
                         {this.createStar(book.rate)}                  
-                        {book.request ? <td style={{paddingLeft: "1.1rem", paddingTop: "0.5rem"}}><img src={edit_no} alt="Edit Disabled" style={{ width: "1.5rem", padding: "0rem"}} /></td>
+                        {book.request.length ? <td style={{paddingLeft: "1.1rem", paddingTop: "0.5rem"}}><img src={edit_no} alt="Edit Disabled" style={{ width: "1.5rem", padding: "0rem"}} /></td>
                           : <td><Button variant="light" onClick={()=>{this.handleEditBook(book)}}>
                             <img src={edit} alt="Edit Book" style={{ width: "1.5rem", padding: "0rem" }} />
                             </Button></td>}                       
@@ -107,15 +106,43 @@ class MyBookstore extends Component{
                               <img src={delIcon} alt="Delete Book" style={{ width: "1.5rem", padding: "0rem" }} />
                             </Button>
                         </td>
-                        <td style={{paddingTop: "0.5rem", paddingLeft: "4rem"}}>{this.createRequestsCol(book.request)}</td>
-                        <td>{book.request > 0  && <Button size="sm" variant="outline-info" style={{ marginTop: "0.5rem"}}>See Request</Button>}</td> 
+                        <td style={{paddingTop: "0.5rem", paddingLeft: "4rem"}}>{this.createRequestsCol(book.request.length)}</td>
                       </tr>
                     ))}
                   </tbody>
                   <Button variant="info" onClick={this.handleAddBook}>Add Books</Button>
                 </Table>
                 {/* <Advertisement/>      */}
-
+                <div style={{marginLeft: "10%", marginTop: "2rem", background: "#eeeeee", width: "25%", textAlign: "center", borderRadius: "4rem", padding: "0.6rem"}}>
+                  <h1 style={{fontSize: "2rem"}}>Received Requests</h1>
+                </div>
+                <Table className="myTable" size="sm" style={{ width: "80%", marginTop: "1.5rem", marginLeft: "auto", marginRight: "auto", paddingTop: "1rem"}}>
+                <thead>
+                    <tr>
+                      <th>Date Received</th>
+                      <th>Title</th>
+                      <th>Sender</th>
+                      <th>Action</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.books.filter(function (book){return book.request.length > 0}).map((book) => (
+                      <tr>
+                        <td>{book.request[0].date}</td>
+                        <td>{book.title}</td>
+                        <td>{book.request[0].sender}</td>
+                        <td>
+                          <Button variant="outline-primary" size="sm" style={{marginLeft:"0.2rem"}} disabled>Accept</Button>
+                          <Button variant="outline-danger" size="sm" style={{marginLeft:"0.2rem"}} disabled>Decline</Button>
+                        </td>
+                        <td>
+                          Pending
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
             </div>
         )
     }
