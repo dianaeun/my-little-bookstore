9b7a09e43299c73e0a0117a3c5f4c6cb669a0811
Hyeon Joon Lee <hyeonjoon.lee@stonybrook.edu>
2020-10-27 03:53:58 +0900

    Navbar slight design modification to allow consistency

--- project_mlb/src/components/NavigationBar.js
+++ project_mlb/src/components/NavigationBar.js
@@ -37,8 +37,8 @@ class MlbNavbar extends Component {
               {this.context.isLogin && <NavLink to="/Profile"> Profile </NavLink> }
             </div>
             <div>
-              {!this.context.isLogin && <NavLink className="nav-link" to="/Login" style={{fontSize: "1rem", background: "#22525F", border: "4px solid #ffffff", borderRadius: "1rem", padding: "0.1 0.4rem", color: "#FAC917"}}>Login</NavLink>}
-              {this.context.isLogin && <Button onClick={this.handleOpen} style={{fontSize: "1rem", fontWeight: "bold", background: "#22525F", border: "4px solid #ffffff", borderRadius: "1rem", padding: "0.1 0.4rem", color: "#FAC917"}}>Logout</Button>}
+              {!this.context.isLogin && <NavLink className="nav-link" to="/Login" style={{fontSize: "1rem", background: "#22525F", border: "4px solid #ffffff", borderRadius: "1rem", color: "#FAC917", paddingTop: "0.37rem", paddingBottom: "0.37rem"}}>Login</NavLink>}
+              {this.context.isLogin && <Button onClick={this.handleOpen} style={{fontSize: "1rem", fontWeight: "bold", background: "#22525F", border: "4px solid #ffffff", borderRadius: "1rem",  color: "#FAC917"}}>Logout</Button>}
             </div>
 
         </Navbar>
