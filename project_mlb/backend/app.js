6285e41415fcb491eba3baa6808510284fbf21b4
diana <dianaeun98@gmail.com>
2020-11-11 21:32:25 +0900

    Update app.js

--- project_mlb/backend/app.js
+++ project_mlb/backend/app.js
@@ -45,5 +45,3 @@ if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === 'staging')
         res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
     });
 }
-    
-    const port = process.env.PORT || 3000;
